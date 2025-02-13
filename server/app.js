09240b85c38872d86098a1414a0a5787b1368b23
ayth-bit <slo.overwatch@gmail.com>
2025-02-14 00:21:04 +0900

    接続オプションの変更

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const mongodbOptions = {
   useUnifiedTopology: true,
   ssl: true,
   sslValidate: false,
-  sslCA: fs.readFileSync(`${__dirname}/home/ec2-user/stabo/global-bundle.pen`)
+  sslCA: fs.readFileSync(`${__dirname}/home/ec2-user/stabo/global-bundle.pem`)
 };
 
 const app = express();
